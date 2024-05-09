fe783d1531e6eec8eea9ca09466f998077b0db69
Michael Lowry <michael.lowry64@gmail.com>
2024-05-09 11:17:26 -0400

    Setup functions to handle math

---
+++ js/script.js
@@ -0,0 +1,28 @@
+let num1;
+let num2;
+let op;
+
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+const operater = (num1, op, num2) => {
+    switch (op) {
+        case '+':
+            return add(num1, num2);
+            break;
+        case '-':
+            return subtract(num1, num2);
+            break;
+        case '*':
+            return multiply(num1, num2);
+            break;
+        case '/':
+            return divide(num1, num2);
+            break;
+        default:
+            console.error("Something has gone very wrong.");
+            break;
+    }
+};
+
