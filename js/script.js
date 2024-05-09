550d0907c35462d8dabda462cb59a9d1c2eae15c
Michael Lowry <michael.lowry64@gmail.com>
2024-05-09 13:17:38 -0400

    Added js button selectors

--- js/script.js
+++ js/script.js
@@ -2,6 +2,30 @@ let num1;
 let num2;
 let op;
 
+//buttons and display selectors
+let buttonC = document.querySelector(".C");
+let display = document.querySelector(".display");
+
+let button7 = document.querySelector("#7");
+let button8 = document.querySelector("#8");
+let button9 = document.querySelector("#9");
+let buttonDivision = document.querySelector("#division");
+
+let button4 = document.querySelector("#4");
+let button5 = document.querySelector("#5");
+let button6 = document.querySelector("#6");
+let buttonMultiplication = document.querySelector("#multiplication");
+
+let button1 = document.querySelector("#1");
+let button2 = document.querySelector("#2");
+let button3 = document.querySelector("#3");
+let buttonSubtraction = document.querySelector("#subtraction");
+
+let button0 = document.querySelector("#0");
+let buttonDecimal = document.querySelector("#decimal");
+let buttonAddition = document.querySelector("#addition");
+let buttonEquals = document.querySelector("#equals");
+
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
@@ -12,7 +36,7 @@ const divide = (a, b) => {
         return undefined;
 };
 
-const operater = (num1, op, num2) => {
+const operate = (num1, op, num2) => {
     switch (op) {
         case '+':
             return add(num1, num2);
